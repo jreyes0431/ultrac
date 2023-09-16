9a9ea2dd6495f075e71a2303fe24fb601696d016
Jose Reyes <jose-31.31@hotmail.com>
2023-09-16 12:09:41 -0500

    fix: unused imports

--- lib/ui/views/views.dart
+++ lib/ui/views/views.dart
@@ -1,10 +1,6 @@
 library views;
 
-import 'dart:convert';
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:ultrac/model/model.dart';
